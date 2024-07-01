691bdd80de60de993220bf1813fa1f32e9014094
ksg-dev <ksg.dev.data@gmail.com>
2024-07-01 15:43:35 -0500

    set up smtp connection for email

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+import datetime as dt
+from dotenv import load_dotenv
+import os
 
-product_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
-# url_product_data = product_url.split("dp/")[1]
-# product_sku = url_product_data.split("?", maxsplit=1)[0]
-#
-# camel_product_url = f"https://camelcamelcamel.com/product/{product_sku}"
-# # print(product_sku)
-#
-# response = requests.get(url=camel_product_url)
-# print(response)
-#
-# # print(content)
+load_dotenv()
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9"
-}
 
-response = requests.get(url=product_url, headers=headers)
-response.raise_for_status()
-webpage = response.text
+def check_price(webpage):
+    soup = BeautifulSoup(webpage, "html.parser")
+    product_name = soup.find(name="span", id="productTitle").getText()
+    total_price = float(soup.find(name="span", class_="aok-offscreen").getText().split("$")[1].strip())
+    return product_name, total_price
 
-soup = BeautifulSoup(webpage, "html.parser")
-total_price = float(soup.find(name="span", class_="aok-offscreen").getText().split("$")[1].strip())
 
-print(total_price)
+def send_email(product_name, product_price, product_url):
+    my_email = os.environ["MY_EMAIL"]
+    app_password = os.environ["EMAIL_PASS"]
+    to_email = os.environ["TO_EMAIL"]
+
+    body = f"{product_name} is now ${product_price}\n{product_url}"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=app_password)
+
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=to_email,
+            msg=f"Subject: PRICE DROP ALERT\n\n{body}"
+        )
+
+
+
+def main():
+    product_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
+
+    response = requests.get(url=product_url, headers=headers)
+    response.raise_for_status()
+    webpage = response.text
+    prod_name, prod_price = check_price(webpage)
+    print(prod_name, prod_price)
+
+if __name__ == "__main__":
+    main()
